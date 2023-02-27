1f9f95a1ea58647bb5e1e7a86aa8c2d8d852075c
baechanghyeon <pek5302@naver.com>
2023-02-27 21:14:02 +0900

    [FEAT] 선택한 재료로 메뉴 찾기 기능 구현(#11)

--- src/components/IngredientSelect/index.tsx
+++ src/components/IngredientSelect/index.tsx
@@ -4,14 +4,21 @@ import { AxiosResponse } from "axios";
 import React, { useState } from "react";
 import SelectItems from "./SelectItems";
 import Refrigerator from "../../assets/refrigerator.svg";
+import { useAppSelector } from "@/store";
 
 const IngredientSelect = () => {
-  const test = ["고구마", "감자"];
-  const test1 = test.join(" ").trim();
+  const SelectIngredients = useAppSelector(
+    (state) => state.ingredientList.SelectIngredients
+  )
+    .join(" ")
+    .trim();
+
   const [menus, setMenus] = useState<string[]>([]);
 
   const handleOnClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    const { data }: AxiosResponse<DataResponse> = await QuetionChat(test1);
+    const { data }: AxiosResponse<DataResponse> = await QuetionChat(
+      SelectIngredients
+    );
     const formatString = data.choices[0].text
       .trim()
       .split("\n")
@@ -23,12 +30,12 @@ const IngredientSelect = () => {
     <div className="flex flex-row h-full">
       <div className="flex flex-col w-[500px] items-center">
         <div className="text-center mt-1">마이 냉장고</div>
-        <div className="flex flex-col w-[300px] h-[220px] m-2 ml-15 bg-yellow-50 justify-center items-center">
+        <div className="flex flex-col w-[300px] h-[220px] m-2 ml-15 bg-yellow-50  items-center overflow-auto scrollbar-hide ">
           {menus.length > 0 ? <p>📖 추천 메뉴</p> : <Refrigerator />}
           {menus.map((item, idx) => (
             <button
               key={`menu_${idx}`}
-              className="w-32 text-left border-2 border-red-50 m-1"
+              className="w-34 text-left border-2 border-red-50 m-1 pr-1 hover:text-white hover:bg-blue-200 ease-in duration-150 "
               value={item}
             >
               💡 {item}
@@ -37,7 +44,7 @@ const IngredientSelect = () => {
         </div>
         <button
           onClick={handleOnClick}
-          className="w-[300px] ml-15 text-center border-2 border-gray-100 cursor-pointer"
+          className="w-[300px] ml-15 text-center border-2 border-gray-100"
         >
           Chat선생 메뉴 추천 받기
         </button>
