0620f7fb88a74c475c865c807eff43fd72b3d613
baechanghyeon <pek5302@naver.com>
2023-02-27 23:31:59 +0900

    [FEAT] 메뉴 클릭 시 레시피 정보 받아오기 기능(API 연결) 구현 (#11)

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,11 +1,20 @@
 import axios from "axios";
 
-export const QuetionChat = (quetionValue: string) => {
+export const QuetionChat = (quetionValue: string, type: string) => {
+  let question = "";
+  if (type === "menu") {
+    question = `(${quetionValue})(을/를) 포함한 메뉴 5개 추천해주세요`;
+  }
+
+  if (type === "recipe") {
+    question = `${quetionValue} 레시피를 알려주세요.`;
+  }
+
   return axios.post(
     String(process.env.NEXT_PUBLIC_API_URL),
     {
       model: "text-davinci-003",
-      prompt: `${quetionValue} 재료를 포함한 메뉴 5개 추천해줘`,
+      prompt: question,
       temperature: 0.5,
       max_tokens: 1024,
       top_p: 0.5,
--- src/components/IngredientSelect/index.tsx
+++ src/components/IngredientSelect/index.tsx
@@ -9,21 +9,42 @@ import { useAppSelector } from "@/store";
 const IngredientSelect = () => {
   const SelectIngredients = useAppSelector(
     (state) => state.ingredientList.SelectIngredients
-  )
-    .join(" ")
-    .trim();
+  );
+  const SelectIngredientsString = SelectIngredients.join(" ").trim();
 
   const [menus, setMenus] = useState<string[]>([]);
+  const [recipe, setRecipe] = useState<string[]>([]);
 
   const handleOnClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    const { name } = e.currentTarget;
+    if (SelectIngredients.length < 1) {
+      alert("1개이상의 재료를 선택해주세요.");
+      return;
+    }
+
+    console.log(name);
+
     const { data }: AxiosResponse<DataResponse> = await QuetionChat(
-      SelectIngredients
+      SelectIngredientsString,
+      name
     );
-    const formatString = data.choices[0].text
-      .trim()
-      .split("\n")
-      .map((item) => item.replace(/^\d+\.\s*/, ""));
-    setMenus(formatString);
+
+    if (name === "menu") {
+      const formatMenuString = data.choices[0].text
+        .trim()
+        .split("\n")
+        .map((item) => item.replace(/^\d+\.\s*/, ""));
+      setMenus(formatMenuString);
+      console.log(menus);
+    }
+
+    if (name === "recipe") {
+      const formatRecipeString = data.choices[0].text
+        .trim()
+        .split("\n")
+        .filter((item) => item !== "");
+      setRecipe(formatRecipeString);
+    }
   };
 
   return (
@@ -32,19 +53,24 @@ const IngredientSelect = () => {
         <div className="text-center mt-1">마이 냉장고</div>
         <div className="flex flex-col w-[300px] h-[220px] m-2 ml-15 bg-yellow-50  items-center overflow-auto scrollbar-hide ">
           {menus.length > 0 ? <p>📖 추천 메뉴</p> : <Refrigerator />}
+
           {menus.map((item, idx) => (
             <button
+              name="recipe"
               key={`menu_${idx}`}
               className="w-34 text-left border-2 border-red-50 m-1 pr-1 hover:text-white hover:bg-blue-200 ease-in duration-150 "
               value={item}
+              onClick={handleOnClick}
             >
               💡 {item}
             </button>
           ))}
         </div>
+
         <button
           onClick={handleOnClick}
-          className="w-[300px] ml-15 text-center border-2 border-gray-100"
+          className="w-[300px] ml-15 text-center border-2 border-gray-100 hover:text-white hover:bg-blut-200"
+          name="menu"
         >
           Chat선생 메뉴 추천 받기
         </button>
