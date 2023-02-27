0258b5d59597c4c5eef2f09b998002763990cdf3
baechanghyeon <pek5302@naver.com>
2023-02-27 16:28:27 +0900

    [FEAT] 재료 선택 리스트에서 삭제 기능 구현(#11)

--- src/components/IngredientSelect/SelectItems.tsx
+++ src/components/IngredientSelect/SelectItems.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import { useAppDispatch, useAppSelector } from "@/store";
 import { FaMinusSquare } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { allDeleteSelectList } from "@/services/ingredientList/slice";
+import {
+  allDeleteSelectList,
+  removeIngredients,
+} from "@/services/ingredientList/slice";
 
 const SelectItems = () => {
   const dispatch = useAppDispatch();
@@ -10,17 +13,13 @@ const SelectItems = () => {
     (state) => state.ingredientList.SelectIngredients
   );
 
-  const allDeleteOnClick = () => {
-    dispatch(allDeleteSelectList());
-  };
-
   return (
     <div className="flex flex-row">
       <div className="flex flex-col m-2 bg-slate-400 w-[180px] ml-2 rounded-md">
         <div className="text-center my-3 text-white text-sm">
           <p>선택한 재료 리스트</p>
           <p
-            onClick={allDeleteOnClick}
+            onClick={() => dispatch(allDeleteSelectList())}
             className="text-[8px] text-right mr-4 hover:text-red-300 cursor-pointer"
           >
             전체 삭제
@@ -28,10 +27,17 @@ const SelectItems = () => {
         </div>
         <div className="flex flex-col w-32 text-white text-sm text-left ml-9 h-[190px] overflow-auto scrollbar-hide">
           {selectIngredients.map((item, idx) => (
-            <div key={idx} className="flex flex-row ">
+            <button
+              onClick={(event) =>
+                dispatch(removeIngredients(event.currentTarget.value))
+              }
+              value={item}
+              key={`select_${idx}`}
+              className="flex flex-row cursor-pointer"
+            >
               <FaMinusSquare className="text-red-300 mt-1 mr-1" />
-              <p className="hover:text-red-300 cursor-pointer">{item}</p>
-            </div>
+              <p className="hover:text-red-300">{item}</p>
+            </button>
           ))}
         </div>
       </div>
